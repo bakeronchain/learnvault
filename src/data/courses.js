d034a59bcd40973c4e0faa3d3a4b96dcda225356
Okorie Chigozie Jehoshaphat <okoriechigozie99@gmail.com>
2026-07-17 18:04:09 +0100

    remove transpiled JS artifacts to fix vite build

--- src/data/courses.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.courses = void 0;
-exports.courses = [
-    { title: "Web3 Fundamentals", track: "Web3", duration: "4 weeks", level: "Beginner" },
-    { title: "Smart Contract Engineering", track: "Solidity", duration: "6 weeks", level: "Intermediate" },
-    { title: "DeFi Protocols", track: "DeFi", duration: "5 weeks", level: "Intermediate" },
-    { title: "Stellar & Soroban Basics", track: "Stellar", duration: "3 weeks", level: "Beginner" },
-];
